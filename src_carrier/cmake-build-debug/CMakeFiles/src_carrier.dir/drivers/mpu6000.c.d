d25c95d1918ac58ba602fbe47d53ff1416038bd7
Atmelfan <gustavp@gpa-robotics.com>
2017-10-15 03:36:59 +0200

    Removed cmake crap

--- src_carrier/cmake-build-debug/CMakeFiles/src_carrier.dir/drivers/mpu6000.c.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/src_carrier.dir/drivers/mpu6000.c.o: \
- /home/atmelfan/documents/ASH-MKII/src_carrier/drivers/mpu6000.c \
- /home/atmelfan/documents/ASH-MKII/src_carrier/drivers/mpu6000.h
--- src_carrier/cmake-build-debug/CMakeFiles/src_carrier.dir/drivers/pca9685.c.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/src_carrier.dir/drivers/pca9685.c.o: \
- /home/atmelfan/documents/ASH-MKII/src_carrier/drivers/pca9685.c \
- /home/atmelfan/documents/ASH-MKII/src_carrier/drivers/pca9685.h \
- /home/atmelfan/documents/ASH-MKII/src_carrier/drivers/i2c/i2c_generic.h \
- /usr/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
- /usr/include/newlib/stdint.h \
- /usr/include/newlib/machine/_default_types.h \
- /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
- /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h
